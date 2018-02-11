4638b73ef4208776207f4e911dfd341d2fff69fa
Mike <mmonthei@icloud.com>
2018-02-11 17:59:58 -0600

    This is the map feature committed branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Monthei Family MAC on 2/11/18.
+//  Copyright © 2018 Monthei Family MAC. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
